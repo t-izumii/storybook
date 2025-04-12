5fefac13aea4e1ea2cbfcfab8951228a702360cb
t-izumii <120708670+t-izumii@users.noreply.github.com>
2025-04-12 23:53:03 +0900

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,30 +1,31 @@
 import type { StorybookConfig } from '@storybook/html-vite';
 import path from 'path';
+import { fileURLToPath } from 'url'; // 追加
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // 追加
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
     '@storybook/addon-docs',
   ],
-  "framework": {
-    "name": "@storybook/html-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/html-vite",
+    options: {}
   },
   viteFinal: async (config) => {
-    // aliasの設定
     config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, '../src'),
     };
-    
     return config;
   },
 };
